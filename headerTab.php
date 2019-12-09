1fd5aca8d829e0c5a738b6fa62a103e867847097
Hussein Alrubaye <hussien89aa@outlook.com>
2019-12-09 08:01:53 -0500

    remve google Analytics

--- headerTab.php
+++ headerTab.php
@@ -12,16 +12,8 @@ require_once "SessionManagement.php";
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111698541-1"></script>
     <link rel="shortcut icon" href="images/crownicon.ico" />
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'UA-111698541-1');
-    </script>
+ 
 
 <style>
 
